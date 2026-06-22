1a131308ef1f2c88e027a66ab7154d8fff764b5f
Risdayanti15 <risdayanti044@gmail.com>
2026-06-22 14:56:03 +0800

    Update config.js

--- Backend/config/config.js
+++ Backend/config/config.js
@@ -1,16 +1,15 @@
 require('dotenv').config();
 
 const dbConfig = {
-  // 🔗 Masing-masing baris ini akan otomatis mengambil data Aiven yang ada di Railway Variables
-  username: process.env.DB_USER,      // Akan membaca 'avnadmin'
-  password: process.env.DB_PASSWORD,  // Akan membaca 'AVNS_fVgef...'
-  database: process.env.DB_NAME,      // Akan membaca 'defaultdb'
-  host: process.env.DB_HOST,          // Akan membaca 'mysql-1a31a69d...'
-  port: process.env.DB_PORT || 3306,  // Akan membaca '27296'
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 3306, // Akan otomatis membaca 27296 jika ada di Railway Variables
   dialect: 'mysql',
   dialectOptions: process.env.PORT ? {
     ssl: {
-      rejectUnauthorized: false // Wajib aktif untuk jabat tangan aman dengan cloud Aiven
+      rejectUnauthorized: false
     }
   } : {}
 };
