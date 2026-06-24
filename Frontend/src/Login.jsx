80705ec3d473afc32cd19fff3555fb90076378b6
Risdayanti15 <risdayanti044@gmail.com>
2026-06-24 12:17:56 +0800

    Update Login.jsx

--- Frontend/src/Login.jsx
+++ Frontend/src/Login.jsx
@@ -18,7 +18,7 @@ function Login() {
       return;
     }
     try {
-      const response = await api.post("/auth/login", { email, password });
+      const response = await api.post("https://rekaweb-rpl-production.up.railway.app/api/auth/login", { email, password });
       localStorage.setItem("token", response.data.token);
       localStorage.setItem("user", JSON.stringify(response.data.user));
       setShowSuccess(true);
